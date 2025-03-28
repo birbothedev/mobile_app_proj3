b0f98f267995c2f410c206bfc806aad710a662ba
birbothedev <123966430+birbothedev@users.noreply.github.com>
2025-03-28 16:07:52 -0400

    incorporated api, brainstorming how to store information

--- app/src/main/java/com/example/project3/MainActivity.kt
+++ app/src/main/java/com/example/project3/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.project3.databinding.ActivityMainBinding
@@ -25,14 +22,19 @@ class MainActivity : AppCompatActivity() {
 
         enableEdgeToEdge()
 
+        retrieveCoinInfo()
+
         binding.coinInfoButton.setOnClickListener {
-            printCoinInfo();
         }
+
     }
 
-    private fun printCoinInfo(){
+    private fun retrieveCoinInfo(){
         val coinURL = "https://api.coincap.io/v2/assets"
         val queue = Volley.newRequestQueue(this)
+
+        var coinNames : Array<String> = emptyArray()
+        var coinSupply : String
 
         // this syntax was recommended by the ide
         val stringRequest = StringRequest (
@@ -45,10 +47,15 @@ class MainActivity : AppCompatActivity() {
                 for (i in 0 until coinArray.length()) {
                     val theCoin : JSONObject = coinArray.getJSONObject(i)
 
-                    Log.i("MainActivity",
-                        "Coin Name: ${theCoin.getString("name")}")
-                    Log.i("MainActivity",
-                        "Coin Symbol: ${theCoin.getString("symbol")}")
+                    // show name coinInfoByName(name)
+
+                    coinNames = arrayOf(theCoin.getString("name"))
+                    coinSupply = theCoin.getString("supply")
+
+//                    Log.i("MainActivity",
+//                        "Coin Name: ${theCoin.getString("name")}")
+//                    Log.i("MainActivity",
+//                        "Coin Symbol: ${theCoin.getString("symbol")}")
                 }
             },
             {
@@ -56,5 +63,40 @@ class MainActivity : AppCompatActivity() {
             })
 
         queue.add(stringRequest)
+    }
+
+//    private fun printCoinInfo(){
+//        val coinURL = "https://api.coincap.io/v2/assets"
+//        val queue = Volley.newRequestQueue(this)
+//
+//        // this syntax was recommended by the ide
+//        val stringRequest = StringRequest (
+//            Request.Method.GET, coinURL,
+//            { response ->
+//                // create json object then get data array from object
+//                val jsonResponse = JSONObject(response)
+//                val coinArray : JSONArray = jsonResponse.getJSONArray("data")
+//
+//                for (i in 0 until coinArray.length()) {
+//                    val theCoin : JSONObject = coinArray.getJSONObject(i)
+//
+//                    // show name coinInfoByName(name)
+//
+//                    Log.i("MainActivity",
+//                        "Coin Name: ${theCoin.getString("name")}")
+//                    Log.i("MainActivity",
+//                        "Coin Symbol: ${theCoin.getString("symbol")}")
+//                }
+//            },
+//            {
+//                Log.i("MainActivity", "That didn't work!")
+//            })
+//
+//        queue.add(stringRequest)
+//    }
+
+    private fun coinInfoByName(name : String) {
+        // drop down name = name
+        // return information
     }
 }
